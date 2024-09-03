9d710836e753093d9dca753263a8f73837ebf977
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-09-03 08:12:34 +0000

    fix config

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,26 +7,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        long lastId = -1;
-        try (BufferedReader reader = Utils.getReader("data.csv")) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+       System.out.println("Hello world!");
 
-        try (BufferedWriter writter = Utils.getWriter("data.csv")) {
-            // writter.newLine();
-            // writter.write("2, user, 123");
-            // writter.flush();
-        
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
+       
     }
 
 }
