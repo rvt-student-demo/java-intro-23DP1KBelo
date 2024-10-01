cdfb7907a4d1714205ca7a7cf8b07a1ad3085c0c
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-10-01 07:48:31 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,40 +10,20 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int num = 0;
-        int even = 0; 
-        System.out.println("Give number: ");
-        while(true) {
-            int input = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("How many times?");
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i<input; i++){
             
-            if (input == -1){
-                break;
-            }
-            sum += input;
-            num++;
-            
-            if (input % 2 == 0){
-                even ++;
-            }
-            
-       }
+            printText();
 
-       System.out.println("Tnx!Bye!");
-       System.out.println("Sum: "+ sum);
-       System.out.println("Numbers: "+ num);
-       System.out.println("Average:" + ((double) sum / num ) );
-       System.out.println("Even:" + even);
-       System.out.println("Odd:" + ((int)num - even));
+        }       
+        
+    }
 
-
-
-
-
-
-
-
-      
+    public static void printText () {
+        System.out.println("In a hole in the ground there lived a method");
     }
 
 }
