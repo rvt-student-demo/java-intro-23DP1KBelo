877ec1a5ae0aba14757562a6190aaef2a26b19b1
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-10-08 09:29:34 +0000

    Advanced astrology (3 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,17 +9,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        printStars(5);
-        printStars(3);
-        printStars(9);
-
-        printSquare(4);
-
-        printRectangle(17, 3);
-
-        printTriangle(4);
+       christmasTree(10);
         } 
     
+    // Star sign
     public static void printStars (int number){
         int n = 0;
         while(n < number){
@@ -53,6 +46,38 @@ public class App
         }
 
     }
+
+    //Advanced astrology
+
+
+    public static void printSpaces(int number){
+        int n = 0;
+        while(n < number){
+            System.out.print(" ");
+            n++; 
+        }
+    }
+
+    public static void printTriangleTwo(int size){
+        for (int i = 1 ; i<=size ;i++){
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height){
+
+        for(int i = 1; i<= height; i++){
+            printSpaces(height - i);
+            printStars((i*2)-1);
+        }
+        for (int i = 0; i<2; i++){
+            printSpaces(height-2);
+            printStars(3);
+        }
+
+    }
+
         
  }
 
