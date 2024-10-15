fa2b972b3d995eababcb2d0a86b7e857ade7248e
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-10-15 08:29:50 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,79 +5,37 @@ import java.io.BufferedWriter;
 
 import java.util.Scanner;
 
+import java.util.ArrayList;
+
 public class App 
 {
     public static void main( String[] args )
     {
-       christmasTree(10);
-        } 
+        Scanner scanner = new Scanner(System.in);
+
+       ArrayList<Integer> intList = new ArrayList<>();
+
+       int input = Integer.valueOf(scanner.nextLine());        
+       intList.add(input);
+        
+        //Izmainām uzdevuma nosaukumu un saskaitām visu elemnu summu, kas atrodas ArrayList'ā
+        int listSum = input;
+
+        while(input != 0 ) {
+            input = Integer.valueOf(scanner.nextLine());        
+            intList.add(input);
+            listSum = listSum + input;
+        }
+        System.out.println(intList.get(0));
+        System.out.println(listSum);
+        
+     }
+     
+     
+     
     
-    // Star sign
-    public static void printStars (int number){
-        int n = 0;
-        while(n < number){
-            System.out.print("*");
-            n++; 
-        }
-        System.out.println("");
-    }
-
-    public static void printSquare(int size){
-      int i = 0;
-        while(i < size){
-            printStars(size);
-            i++;
-        }
-    }
-
-    public static void printRectangle(int width, int height){
-       int i = 0;
-        while(i < height){
-            printStars(width);
-            i++;
-        }
-    }
-
-    public static void printTriangle(int size){
-        int i = 1;
-        while(i < size + 1){
-            printStars(i);
-            i++;
-        }
-
-    }
-
-    //Advanced astrology
-
-
-    public static void printSpaces(int number){
-        int n = 0;
-        while(n < number){
-            System.out.print(" ");
-            n++; 
-        }
-    }
-
-    public static void printTriangleTwo(int size){
-        for (int i = 1 ; i<=size ;i++){
-            printSpaces(size - i);
-            printStars(i);
-        }
-    }
-
-    public static void christmasTree(int height){
-
-        for(int i = 1; i<= height; i++){
-            printSpaces(height - i);
-            printStars((i*2)-1);
-        }
-        for (int i = 0; i<2; i++){
-            printSpaces(height-2);
-            printStars(3);
-        }
-
-    }
+}
 
         
- }
+ 
 
