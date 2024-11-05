0e3d364641f31f90704f03b4c3790960a3b08fbb
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-11-05 07:31:37 +0000

    Person

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,24 +9,14 @@ import java.util.ArrayList;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-      int[] valA = {13, -22, 82, 17};
-      int[] valB = {0, 0, 0, 0};
+  public static void main( String[] args ){
+    
+    Person per1 = new Person("John");
 
-
-      for(int i = 0; i < valA.length; i++){
-        valB[i] = 25 - valA[i];
-      }
-
-      System.out.println("ValA:" + valA[0] + " " + valA[1]+ " " + valA[2]+ " " + valA[3]);
-
-      System.out.println("ValA:" + valB[0] + " " + valB[1]+ " " + valB[2]+ " " + valB[3]);
-
-      System.out.println("Sum: " + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " + (valA[2]+valB[2]) + " "+ (valA[3]+valB[3]));
+    per1.printPerson();
+    for(int i = 0; i<35 ; i++){
+      per1.growOlder();
     }
+    per1.printPerson();
+  }
 }
-
-  
- 
-
