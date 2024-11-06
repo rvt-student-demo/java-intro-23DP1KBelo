cf2074952ab36ccb8d25e5ea185625ad6c07a51b
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-11-06 13:44:34 +0000

    Statistics part 2

--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -2,17 +2,29 @@ package lv.rvt;
 
 public class Statistics {
     private int count;
+    private double sum;
 
     public Statistics() {
         this.count = count;
+        this.sum = sum;
     }
 
     public void addNumber(int number) {
         this.count ++;
+        this.sum += number;
         
     }
 
     public int getCount() {
         return this.count;
     }
+    public double sum() {
+        return this.sum;
+
+    }
+
+    public double average() {
+        return this.sum / this.count;
+    }
+
 }
