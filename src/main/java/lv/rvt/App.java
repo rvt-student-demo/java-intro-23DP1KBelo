70c01ecfab9608a215f3a370cc2bcf7f2294dc1c
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-11-06 14:11:03 +0000

    Statistics Part 4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -13,18 +13,29 @@ public class App
     Scanner scanner = new Scanner(System.in);
     int i = 0;
 
-    Statistics viens= new Statistics();
+    Statistics sum= new Statistics();
+    Statistics sumOdd= new Statistics();
+    Statistics sumEven= new Statistics();
+
 
     while (true){
       if ( i == -1){
          break;
       } else{
-        viens.addNumber(i);
+        sum.addNumber(i);
         i = Integer.valueOf(scanner.nextLine());
+        if (i % 2 == 0){
+          sumEven.addNumber(i);
+        }
+        if(i % 2 == 1){
+          sumOdd.addNumber(i);
+        }
       }
     }
 
-    System.out.println("Sum: " + viens.sum());
+    System.out.println("Sum: " + sum.sum());
+    System.out.println("Sum of even number: " + sumEven.sum());
+    System.out.println("Sum of odd number: " + sumOdd.sum());
 
   }
 
