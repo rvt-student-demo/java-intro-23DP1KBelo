1894b6e6f6d64c03a94b8b8e187550005184052a
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2024-12-03 08:43:42 +0000

    PersonManager(Part 1)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,11 +8,10 @@ public class App
   public static void main(String[] args) throws Exception {
 
     ArrayList<Person> persons = PersonManager.getPersonList();
-    
+
     for (Person person : persons){
       System.out.println(person);
     }
-    
   }
 }
   
