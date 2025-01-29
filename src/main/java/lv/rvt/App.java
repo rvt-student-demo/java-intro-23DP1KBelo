1ac8376e6640189666a9195061e1988c249aca78
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2025-01-29 13:38:00 +0000

    part 3: Student toString

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,16 +6,13 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javax.swing.Box;
-
 public class App {
-  public static void main(String[] args){
-    student ollie = new student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-    System.out.println(ollie);
-    System.out.println("Study credits " + ollie.credit());
-    ollie.Study();
-    System.out.println("Study credits "+ ollie.credit());
+    public static void main(String[] args){
+        student ollie = new student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+        System.out.println(ollie);
+        ollie.study();
+        System.out.println(ollie);
+    }
   }
-}
-
+  
   
