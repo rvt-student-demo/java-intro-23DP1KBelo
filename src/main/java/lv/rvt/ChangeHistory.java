96d361730e6b2e3cb663ce1723de907bf2695eaf
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2025-01-30 13:46:02 +0000

    part4

--- src/main/java/lv/rvt/ChangeHistory.java
+++ src/main/java/lv/rvt/ChangeHistory.java
@@ -12,10 +12,42 @@ public class ChangeHistory {
     public void add(double status){
         this.records.add(status);
     }
-    
+
+    public double maxValue(){
+        double max = this.records.get(0);
+        for(Double value : records){
+            if(value > max){
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public double minValue(){
+        double min = this.records.get(0);
+        for(Double value : records){
+            if(value > min){
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public double avarage(){
+        double sum = 0;
+        int sk = 0;
+        for(double value : records){
+            sum = value + sum;
+            sk ++;
+        }
+        return sum / sk;
+    }
+
+
     public void clear(){
         this.records.clear();
     }
+
     public String toString(){
         return this.records.toString();
     }
