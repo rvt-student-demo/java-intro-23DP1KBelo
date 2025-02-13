fa21e62f9262b5c3172ad6f7e14bdaf5c3e4f134
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2025-02-13 12:45:04 +0000

    part 4 A Box is packable too!

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         boxes box = new boxes(10);
-
+        boxes box2 = new boxes(20);
         box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
         box.add(new Book("Robert Martin", "Clean Code", 1));
         box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
@@ -17,8 +17,10 @@ public class App {
         box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
         box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
         box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+
+        box2.add(box);
     
-        System.out.println(box);
+        System.out.println(box2);
     
     }
 }
