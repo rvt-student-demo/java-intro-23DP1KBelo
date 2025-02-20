141965d51e5530503a64d6ea3c25bd101ae96d24
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2025-02-20 13:27:12 +0000

    color change

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,20 +8,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        boxes box = new boxes(10);
-        boxes box2 = new boxes(20);
-        box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
-        box.add(new Book("Robert Martin", "Clean Code", 1));
-        box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
-    
-        box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
-        box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
-        box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
-
-        box2.add(box);
-    
-        System.out.println(box2);
-    
+        System.out.println("\033[1;32m");
+        System.out.println("hello world!");
     }
 }
   
