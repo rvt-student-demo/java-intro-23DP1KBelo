ea4c6a37af1236fe5f86dd5ecdc2cc0b3e05c376
23DP1KBelo <166118176+23DP1KBelo@users.noreply.github.com>
2025-02-20 13:47:01 +0000

    flag

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,10 +10,38 @@ import lv.rvt.tools.ConsoleColors;
 
 public class App {
     public static void main(String[] args){
+
+
         System.out.print(
-        ConsoleColors.YELLOW_BOLD+    
-        ConsoleColors.BLUE_BACKGROUND+
-        "hello world!");
+        ConsoleColors.BLUE_BACKGROUND +
+        "               "+
+        ConsoleColors.BLUE_BACKGROUND +
+        "               "+
+        ConsoleColors.BLUE_BACKGROUND +
+        "             \n"+
+        ConsoleColors.GREEN_BACKGROUND +
+        "               "+
+        ConsoleColors.GREEN_BACKGROUND +
+        "               "+
+        ConsoleColors.GREEN_BACKGROUND +
+        "             \n"+
+        ConsoleColors.BLUE_BACKGROUND +
+        "               "+
+        ConsoleColors.BLUE_BACKGROUND +
+        "               "+
+        ConsoleColors.BLUE_BACKGROUND +
+        "             \n"+
+
+        ConsoleColors.RESET);
+
+        //BACKGROUND + COLOR + RESET
+        // System.out.print(
+        //     ConsoleColors.YELLOW_BOLD+    
+        //     ConsoleColors.BLUE_BACKGROUND+
+        //     "hello world!"
+        //     +ConsoleColors.RESET);
+
+
     }
 }
   
